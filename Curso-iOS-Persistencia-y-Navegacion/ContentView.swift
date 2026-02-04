839381f76d79aa3da397313e73bec9e19ed7de32
Eduardo <eduardo@microsoft.com>
2026-02-04 12:25:24 +0100

    Initial Commit

---
+++ Curso-iOS-Persistencia-y-Navegacion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-iOS-Persistencia-y-Navegacion
+//
+//  Created by Equipo 2 on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-iOS-Persistencia-y-Navegacion/Curso_iOS_Persistencia_y_NavegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_Persistencia_y_NavegacionApp.swift
+//  Curso-iOS-Persistencia-y-Navegacion
+//
+//  Created by Equipo 2 on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_Persistencia_y_NavegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
