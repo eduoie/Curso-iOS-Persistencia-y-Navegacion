a2b489073e9736bd63a855dac80f06e888037434
Eduardo <eduardo@microsoft.com>
2026-02-04 14:16:47 +0100

    Vista con persistencia

--- Curso-iOS-Persistencia-y-Navegacion/ContentView.swift
+++ Curso-iOS-Persistencia-y-Navegacion/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
+enum ClavesStorage {
+    static let ultimoLogin = "ultimo_login"
+}
+
+extension String {
+    static let ultimoLogin = "ultimo_login"
+}
+
 struct ContentView: View {
+
+    @AppStorage("usuario") private var nombreUsuario = "Invitado"
+    @AppStorage("musicaActivada") private var musicaActivada: Bool = false
+    
+    @State private var ultimaFechaLogin = "Nunca"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Datos de usuario (persistentes)") {
+                // Persiste el valor automáticamente al modificarlo
+                TextField("Tu nombre", text: $nombreUsuario)
+                Toggle("Música activada", isOn: $musicaActivada)
+            }
+            
+            Section("Hora de acceso/registro") {
+                Text("Último acceso: \(ultimaFechaLogin)")
+                
+                Button("Guardar fecha de login") {
+                    guardarFechaLogin()
+                }
+                
+                Button("Borrar nuestro rastro de login") {
+                    borrarFechaLogin()
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            cargarFechaLogin()
+        }
+    }
+    
+    func guardarFechaLogin() {
+        let fechaFormateada: String = Date().formatted(date: .abbreviated, time: .standard)
+        // Guardamos en UserDefaults
+        UserDefaults.standard.set(fechaFormateada, forKey: ClavesStorage.ultimoLogin)
+        
+        ultimaFechaLogin = fechaFormateada
+    }
+    
+    func cargarFechaLogin() {
+        if let fechaLogin = UserDefaults.standard.string(forKey: .ultimoLogin) {
+            ultimaFechaLogin = fechaLogin
+        }
+    }
+    
+    func borrarFechaLogin() {
+        UserDefaults.standard.removeObject(forKey: "ultimo_login")
+        ultimaFechaLogin = "Borrado registro de login"
     }
 }
 
